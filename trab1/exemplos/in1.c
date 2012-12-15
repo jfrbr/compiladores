e2d23780b756284a387a3ef61da498c07cfd81e4
jfrbr <juan.franca@gmail.com>
2012-12-15 00:16:43 -0200

    esqueci dos exemplos

--- trab1/exemplos/in1.c
+++ trab1/exemplos/in1.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 
 int main(){
+	int a;
+	int b;
+	a = 10;
+	b = 5;
+	
+	if ( a > b){
+		a = a + b;
+	}
+	else{
+		a = a-b;
+	}
+
+	if ( a % 2 == 1|| a % 3 == 2){
+		a = a-2;
+	}
+	
+	
 	return 0;
 }
