7583ce0e3305b36dadece36de4337fa8f1fe213c
Andre Siviero <altsiviero@gmail.com>
2012-12-20 12:15:18 -0200

    Versao Entregue

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all:
 	flex -o trab1.c trab1.l
 	gcc trab1.c -o trab1 -lfl
-
+	latex -output-directory=doc/ doc/doc_trab1.tex 
+	
 clean:	
-	rm trab1
+	rm trab1 doc/doc_trab1.dvi doc/doc_trab1.aux doc/doc_trab1.log
 
--- exemplos/in5.c
+++ exemplos/in5.c
@@ -4,19 +4,25 @@ void debug() {
 	printf("aooooo trem que pula!\n");
 }
 
+
+
 int main(){
-	float a;
-	float b;
+	int a;
+	int b;
 	a = 10;
-	b = 5.1e9;
+	b = 5;
+	int define = 2;
 	
 	a += 1;
 	b /= 2;
 	
 	unsigned c = 1;
 	
-	c = a|b;
-	c = a&b;
+	c = a^b;
+	c &= b;
+	c = 0;
+	c ^= 1;
+	printf("debug = %d\n",define);
 	// Affe maria
 	debug();
 	debug();
