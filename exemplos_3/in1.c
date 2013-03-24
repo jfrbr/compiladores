b78eee870e2bbb94ea3a65163b4be9269e44f5ed
juan fr <juan.franca@gmail.com>
2013-03-24 19:36:27 -0300

    Exemplos corretos

---
+++ exemplos_3/in1.c
@@ -0,0 +1,10 @@
+int main(){
+
+	int a;
+
+	for (a=0;a<10;a++){
+		printf("%d",a);
+	}
+	
+	return 0;
+}
---
+++ exemplos_3/in3.c
@@ -0,0 +1,17 @@
+int main(){
+	
+	int a;
+	char c;
+	c = 'a';
+
+	for (a=0;a<10;a++){
+		c += a;
+	}
+
+	while (a < 10){
+		printf("%d",a);
+	}
+
+	return -1;
+
+}
